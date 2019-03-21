a27a3480579629ff28117be806f5027743652b72
Mateusz Grochala <mateusz.grochala@yahoo.dk>
2019-03-21 10:38:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Resources for Laravel
+
+## Install
+
+Via Composer
+
+``` bash
+$ composer require morningtrain/laravel-resources
+```
+
+## Usage
+TODO
+
+## Credits
+
+- [Morning Train](https://morningtrain.dk/)
+
---
+++ src/LaravelResourcesServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace MorningTrain\Laravel\Resources;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelResourcesServiceProvider extends ServiceProvider
+{
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+}

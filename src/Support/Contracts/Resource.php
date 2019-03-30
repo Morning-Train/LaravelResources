46fc4a346fc6e0bd4164c5f888a14e3ae11359a7
Bjarne Bonde <bb@morningtrain.dk>
2019-03-30 09:54:16 +0100

    cleanup

--- src/Support/Contracts/Resource.php
+++ src/Support/Contracts/Resource.php
@@ -4,11 +4,9 @@ namespace MorningTrain\Laravel\Resources\Support\Contracts;
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
-use MorningTrain\Laravel\Resources\Support\Traits\HasOperations;
 
 abstract class Resource
 {
-    use HasOperations;
 
     public $namespace;
     public $name;
@@ -17,21 +15,6 @@ abstract class Resource
     {
         $this->name = static::getName();
     }
-
-    /////////////////////////////////
-    /// Request helpers
-    /////////////////////////////////
-
-    // TODO - Move to Operation + update in favor of permissions package
-    //public static function getPermittedOperations(bool $trans = false)
-    //{
-    //    $user = \Auth::user();
-    //    $ops = static::getOperations($trans);
-
-    //    return Arr::where($ops, function ($val, $key) use ($user) {
-    //        return $user->can($key, static::instance());
-    //    });
-    //}
 
     /////////////////////////////////
     /// Basic helpers
@@ -87,6 +70,111 @@ abstract class Resource
                 }
             }
         });
+    }
+
+    /////////////////////////////////
+    /// Operations
+    /////////////////////////////////
+
+    protected static $_cached_operations = [];
+
+    /// Should be overridden if providing operation class names
+    protected static $operations = [];
+
+    public function operations()
+    {
+        /// Should be overridden if providing operation instances instead of classes
+        return [];
+    }
+
+    public function operation($slug)
+    {
+        if ($this->hasOperation($slug)) {
+            return $this->getOperations()[$slug];
+        }
+
+        throw new \Exception("Tried to get operation ($slug), but it is not found on resource (" . $this->name . ")");
+
+    }
+
+    public function getOperations()
+    {
+        if (!isset(static::$_cached_operations[get_class($this)])) {
+
+            $raw_class_operations = static::$operations;
+
+            $operations = [];
+
+            if (is_array($raw_class_operations) && !empty($raw_class_operations)) {
+                foreach ($raw_class_operations as $key => $operation) {
+
+                    if (!class_exists($operation)) {
+                        throw new \Exception("Supplied operation ($operation) on resource (" . get_class($this) . "), but it is not a class!");
+                    }
+
+                    if (is_int($key)) {
+                        $key = Str::snake(strtolower(class_basename($operation)));
+                    }
+                    $operations[$key] = new $operation;
+                }
+            }
+
+            $instance_operations = $this->operations();
+
+            if (is_array($instance_operations) && !empty($instance_operations)) {
+                foreach ($instance_operations as $key => $operation) {
+                    if (is_int($key)) {
+                        $key = Str::snake(strtolower(class_basename(get_class($operation))));
+                    }
+                    $operations[$key] = $operation;
+                }
+            }
+
+            if (empty($operations)) {
+                throw new \Exception('Looking for operations on resource: ' . get_class($this) . ', but none was found!');
+            }
+
+            static::$_cached_operations[get_class($this)] = $operations;
+        }
+
+        return static::$_cached_operations[get_class($this)];
+    }
+
+    public function hasOperations()
+    {
+        return is_array($this->getOperations()) && !empty($this->getOperations());
+    }
+
+    public function hasOperation($operationName)
+    {
+        return isset($this->getOperations()[$operationName]);
+    }
+
+    public function configureOperations()
+    {
+        if ($this->hasOperations()) {
+            foreach ($this->getOperations() as $operationName => $operation) {
+
+                /// Set current resource on operation
+                $operation->resource($this);
+                $operation->name($operationName);
+
+                // Method to be called for this specific operation
+                $method = Str::camel('configure_' . $operation->name . '_operation');
+                $callable = [$this, $method];
+                if (method_exists($this, $method) && is_callable($callable)) {
+                    call_user_func($callable, $operation);
+                }
+
+                /// Generic function to always be called on the operation
+                $method = Str::camel('configure_operation');
+                $callable = [$this, $method];
+                if (method_exists($this, $method) && is_callable($callable)) {
+                    call_user_func($callable, $operation);
+                }
+
+            }
+        }
     }
 
 }

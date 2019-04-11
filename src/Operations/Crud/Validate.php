c932262f110e4d965ae43b1dccc06bb8ab6686fa
Mateusz Grochala <mateusz.grochala@yahoo.dk>
2019-04-11 13:30:47 +0200

    + simple Validate Operation

---
+++ src/Operations/Crud/Validate.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace MorningTrain\Laravel\Resources\Operations\Crud;
+
+
+use Illuminate\Http\Request;
+use MorningTrain\Laravel\Resources\Support\Contracts\EloquentOperation;
+
+class Validate extends EloquentOperation
+{
+    const ROUTE_METHOD = 'post';
+
+    protected $strict = false;
+
+    public function handle($model = null)
+    {
+        /** @var Request $request */
+        $request = request();
+
+        $this->performValidation($model, $request, !$this->strict);
+
+        return [
+            'model' => $request->all(),
+        ];
+
+    }
+
+    public function prepare($parameters)
+    {
+        $this->data = $this->onEmptyModel();
+    }
+
+    public function strict(bool $val = null)
+    {
+        return $this->genericGetSet('strict', $val);
+    }
+
+    public function onEmptyModel()
+    {
+        return $this->getEmptyModelInstance();
+    }
+
+}

b7f05b6496eaa3e1802bcc1e87a430f021b5dc17
Bjarne Bonde <bb@morningtrain.dk>
2019-11-20 14:12:34 +0100

    Updated EloquentOperation to check if route actually exists before using it

--- src/Support/Contracts/EloquentOperation.php
+++ src/Support/Contracts/EloquentOperation.php
@@ -31,7 +31,11 @@ abstract class EloquentOperation extends Operation
     {
         return [
             QueryModel::create()->model($this->model)->filters($this->filters)->operation($this),
-            QueryToInstance::create()->keyValue(request()->route()->parameter($this->getModelClassName()))->operation($this),
+            QueryToInstance::create()->keyValue(
+                (request()->route() !== null)
+                    ? request()->route()->parameter($this->getModelClassName())
+                    : null
+            )->operation($this),
             TransformToView::create()->appends($this->appends),
         ];
     }

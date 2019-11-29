f1754d8a215b4364f361d0592c980b5544c92c0f
Mateusz Grochala <mateusz.grochala@yahoo.dk>
2019-11-29 10:24:31 +0100

    + success() Respondable method

--- src/Support/Traits/Respondable.php
+++ src/Support/Traits/Respondable.php
@@ -4,6 +4,7 @@ namespace MorningTrain\Laravel\Resources\Support\Traits;
 
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 trait Respondable
 {
@@ -22,6 +23,15 @@ trait Respondable
     protected function forbidden($message)
     {
         throw new AccessDeniedHttpException($message);
+    }
+
+    /**
+     * @param $message
+     * @param int $statusCode
+     */
+    protected function success($message, int $statusCode = 200)
+    {
+        throw new HttpException($statusCode, $message);
     }
 
 }

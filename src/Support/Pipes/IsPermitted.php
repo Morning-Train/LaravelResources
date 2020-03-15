e137ceab902b6675a8c0dbd7d6528568c0a97d84
Bjarne Bonde <bb@morningtrain.dk>
2020-03-15 12:57:56 +0100

    IsPermitted pipe updated to use Gate allow checks for guest users

--- src/Support/Pipes/IsPermitted.php
+++ src/Support/Pipes/IsPermitted.php
@@ -4,7 +4,7 @@ namespace MorningTrain\Laravel\Resources\Support\Pipes;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 use MorningTrain\Laravel\Resources\ResourceRepository;
 
 class IsPermitted extends Pipe
@@ -38,13 +38,8 @@ class IsPermitted extends Pipe
 
     protected function getPermissionsMeta($collection)
     {
-        if (!Auth::check()) {
-            return [];
-        }
 
-        $user = Auth::user();
-
-        $res = $collection->mapWithKeys(function ($model) use ($user) {
+        $res = $collection->mapWithKeys(function ($model) {
 
             if ($model === null || !($model instanceof Model)) {
                 return [];
@@ -53,8 +48,8 @@ class IsPermitted extends Pipe
             return [
                 $model->getKey() =>
                     collect(ResourceRepository::getModelPermissions($model))
-                        ->filter(function ($operation) use ($model, $user) {
-                            return $user->can($operation, $model);
+                        ->filter(function ($operation) use ($model) {
+                            return Gate::allows($operation, $model);
                         })
                         ->values()
                         ->all(),

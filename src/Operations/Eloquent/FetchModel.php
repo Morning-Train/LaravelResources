6c769646c2523f5744c31b96e0457ee86ebddefb
Bjarne Bonde <bb@morningtrain.dk>
2020-09-14 09:57:02 +0200

    Added FetchModel operation to fetch a single instance without requiring an instance

---
+++ src/Operations/Eloquent/FetchModel.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace MorningTrain\Laravel\Resources\Operations\Eloquent;
+
+use MorningTrain\Laravel\Resources\Support\Contracts\EloquentOperation;
+use MorningTrain\Laravel\Resources\Support\Pipes\Eloquent\QueryModel;
+use MorningTrain\Laravel\Resources\Support\Pipes\Eloquent\QueryToModel;
+use MorningTrain\Laravel\Resources\Support\Pipes\TransformToView;
+
+class FetchModel extends EloquentOperation
+{
+
+    const ROUTE_METHOD = 'get';
+
+    protected function beforePipes()
+    {
+        return [
+            QueryModel::create()->model($this->model)->filters($this->filters),
+            QueryToModel::create(),
+            TransformToView::create()->appends($this->appends),
+        ];
+    }
+
+}

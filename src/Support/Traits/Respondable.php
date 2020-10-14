4803f80f6650a642c10921b6c409f0da40d1a936
Bjarne Bonde <bb@morningtrain.dk>
2020-10-14 13:20:46 +0200

    Added a respond method to Repondable trait

--- src/Support/Traits/Respondable.php
+++ src/Support/Traits/Respondable.php
@@ -2,6 +2,8 @@
 
 namespace MorningTrain\Laravel\Resources\Support\Traits;
 
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
@@ -32,6 +34,16 @@ trait Respondable
     protected function success($message, int $statusCode = 200)
     {
         throw new HttpException($statusCode, $message);
+    }
+    
+    /**
+     * Throw a new response exception.
+     *
+     * @param  \Symfony\Component\HttpFoundation\Response  $response
+     * @return void
+     */
+    protected function respond(Response $response) {
+        throw new HttpResponseException($response);
     }
 
 }

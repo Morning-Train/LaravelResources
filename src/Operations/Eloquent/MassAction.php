a7a4e09c5b85d9a6d1809a7da9d0b12a3352ecef
ipsen <pi@morningscore.io>
2021-03-10 16:07:18 +0100

    Rewrote MassAction operation

--- src/Operations/Eloquent/MassAction.php
+++ src/Operations/Eloquent/MassAction.php
@@ -2,73 +2,47 @@
 
 namespace MorningTrain\Laravel\Resources\Operations\Eloquent;
 
-use Illuminate\Support\Collection;
+use MorningTrain\Laravel\Resources\Support\Pipes\Eloquent\TriggerOnModelsInCollection;
 use MorningTrain\Laravel\Filters\Filters\Filter;
-use MorningTrain\Laravel\Resources\Support\Pipes\Collections\KeyBy;
 use MorningTrain\Laravel\Resources\Support\Pipes\Validates;
-use MorningTrain\Laravel\Resources\Support\Traits\HasRules;
 
-class MassAction extends Action
+class MassAction extends Index
 {
 
-    use HasRules;
+    const ROUTE_METHOD = 'POST';
 
-    const ROUTE_METHOD = 'post';
+    protected $trigger = null;
 
-    public function __construct()
+    public function trigger($value = null)
     {
+        $this->trigger = $value;
 
-        $this->filters([
+        return $this;
+    }
+
+    public function getFilters()
+    {
+        return [
             Filter::create()->when('ids', function ($q, $ids) {
                 $q->whereIn($this->getModelKeyName(), $ids);
-            })->missing('ids', function ($q) {
-                abort(401, 'Do not be stupid!');
             })
-        ]);
-
-        $this->rules([
-            'ids' => 'required|array'
-        ]);
-
+        ];
     }
 
     public function beforePipes()
     {
-        return array_merge(
-            [
-                Validates::create()->rules($this->rules)
-            ],
-            parent::beforePipes(),
-            [
-                KeyBy::create()->model($this->model)
-            ]
-        );
+        return array_merge(parent::beforePipes(), [
+            Validates::create()->rules([
+                'ids' => 'required|array'
+            ])
+        ]);
     }
 
-    public function expectsCollection()
+    protected function pipes()
     {
-        return true;
+        return [
+            TriggerOnModelsInCollection::create()->trigger($this->trigger)
+        ];
     }
 
-    public function performTrigger($collection = null)
-    {
-
-        $trigger = $this->trigger;
-
-        if ($collection === null || !($collection instanceof Collection)) {
-            abort(401, 'Unable to perform mass action - Did not get expected Collection');
-        }
-
-        if ($collection->isEmpty()) {
-            return abort(401, 'No entries found to apply mass action to');
-        }
-
-        $collection->transform(function ($model) use ($trigger) {
-            return $trigger instanceof \Closure ?
-                $trigger($model) :
-                $model->{$trigger}();
-        });
-
-        return $collection;
-    }
 }
